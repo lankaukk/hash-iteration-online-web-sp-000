e698830d98bbbca09b7002ab70ffc6f043630b2a
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 02:28:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,8 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |name, age|
-    puts ""
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 
 
